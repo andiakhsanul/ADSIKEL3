dd7dd5e17f9c90124ccdd453b1df305c904ec457
andi akhsanul <116082109+andiakhsanul@users.noreply.github.com>
2023-05-23 23:39:09 +0700

    Create br.php

---
+++ br.php
@@ -0,0 +1,22 @@
+<?php
+
+class Barang
+{
+    private $nama;
+    private $spec
+
+    public function __construct(Type $var = null) {
+        $this->var = $var;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+?>
