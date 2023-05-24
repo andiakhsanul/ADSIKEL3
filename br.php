d1d10b380270077534af7f180932474fc22ea229
andi akhsanul <116082109+andiakhsanul@users.noreply.github.com>
2023-05-24 11:55:11 +0700

    Update br.php

--- br.php
+++ br.php
@@ -1,17 +1,32 @@
 <?php
-
+include_once("as.php");
+include_once("p.php");
 class Barang
 {
-    private $nama;
-    private $spec
+    private  $nama;
+    private  $spec;
 
-    public function __construct(Type $var = null) {
-        $this->var = $var;
+     public function __construct($nama,$spec) {
+        $this->nama = $nama;
+        $this->spec=$spec;
     }
-    
+    public function getNama()
+    {
+        return $this->nama;
+    }
+    public function getSpec()
+    {
+        return $this->spec;
+    }
+    public function setSpec(String $a)
+    {
+        $this->spec= $a;
+    }
+ 
+
+
+
 }
-
-
 
 
 
